9da9dc7cb24ac29a623685a0d009b683aad89baa
SughraRasheed <159168716+SughraRasheed@users.noreply.github.com>
2024-04-27 16:21:31 +0500

    This is my word counter

---
+++ main.js
@@ -0,0 +1,9 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{
+        name: "Sentence",
+        type: "input",
+        message: "Write your sentence to count the words"
+    }]);
+const words = answer.Sentence.trim().split(" ");
+console.log(words);
+console.log(`Total words of your sentence are: ${words.length}`);
---
+++ main.ts
@@ -0,0 +1,12 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+
+const answer : {
+    Sentence : string } = await inquirer.prompt([{
+        name : "Sentence",
+        type : "input",
+        message : "Write your sentence to count the words"
+    }])
+    const words = answer.Sentence.trim().split(" ")
+    console.log(words);
+    console.log(`Total words of your sentence are: ${words.length}`);
